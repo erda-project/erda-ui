fe0bcef1c9d045f320b7a89123fae2c270ce71fe
Zero <mobius_pan@yeah.net>
2021-04-26 19:35:55 +0800

    unit test (#39)
    * fix: fix header is null
    
    * test: unit test
    
    * test: mock iconfont.js
    
    * test: WIP unit test
    
    * test: WIP unit test
    
    * test: WIP unit test
    
    * fest: update licenses
    
    * test: statement
    
    * test: merge upstream to Zero
    
    * chore: add codecov
    
    * feat: merge upstream to local
    
    * feat: remove zero's codecov badge
    
    * feat: add npm conf
    
    * feat: fix dependencies
    
    * feat: fix target
    
    * chore: use codecov bash
    
    * chore: use codecov bash
    
    * chore: reset first version
    
    * feat: add codecov badge
    
    * test: add test case
    
    * feat: test case
    
    * test: add test case
    
    * test: remove snapshots
    
    * chore: add codecov config
    
    * test: back to top
    
    * test: add case

--- shell/app/common/utils/go-to.tsx
+++ shell/app/common/utils/go-to.tsx
@@ -46,7 +46,7 @@ interface IOptions {
 export const goTo = (pathStr: string, options?: IOptions) => {
   const { replace = false, forbidRepeat = false, jumpOut = false, query, ...rest } = options as IOptions || {};
   let _path = '';
-  
+
   if (/^(http|https):\/\//.test(pathStr)) { // 外链
     if (jumpOut) {
       window.open(pathStr);
@@ -118,7 +118,7 @@ export enum pages {
   freshMan = '/{orgName}/freshMan',
   notFound = '/{orgName}/notFound',
 
-  //workBench
+  // workBench
   orgRoot = '/{orgName}',
   orgList = '/{orgName}/org-list',
   workBenchRoot = '/{orgName}/workBench/projects',
@@ -129,7 +129,7 @@ export enum pages {
   workBenchMyInitiate = '/{orgName}/workBench/approval/my-initiate',
   workBenchMyInitiateWait = '/{orgName}/workBench/approval/my-initiate/WaitApprove',
   workBenchPublisher = '/{orgName}/workBench/publisher',
-  
+
 
   // project
   project = '/{orgName}/workBench/projects/{projectId}',
@@ -283,7 +283,6 @@ export enum pages {
   orgCenterAnnouncement = '/{orgName}/orgCenter/announcement',
   orgCenterSafety = '/{orgName}/orgCenter/safety',
 
-  
 
   dataCenterNotifyGroup = '/{orgName}/orgCenter/setting/detail?tabKey=notifyGroup',
   dataCenterSetting = '/{orgName}/orgCenter/setting/detail',
