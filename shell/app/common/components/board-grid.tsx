6fc5d8aab63e1c8a47973b1840858b2c425eb330
lsh <30284157+michael8512@users.noreply.github.com>
2021-05-06 18:04:38 +0800

    fix the org problem of board-grid url (#96)
    * ...
    
    * fix: fix the org problem in border-grid url
    
    * WIP: remove the empty line

--- shell/app/common/components/board-grid.tsx
+++ shell/app/common/components/board-grid.tsx
@@ -12,12 +12,15 @@
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import * as React from 'react';
+import { setApiWithOrg } from 'common/utils';
 import {
   BoardGrid as DCBoardGrid,
   PureBoardGrid as DCPureBoardGrid,
   setLocale,
   DC,
 } from '@terminus/dashboard-configurator';
+import produce from 'immer';
+import { forEach } from 'lodash';
 
 export const BoardGrid = ({ ...restProps }: DC.BoardGridProps) => {
   const locale = window.localStorage.getItem('locale') || 'zh';
@@ -28,6 +31,12 @@ export const BoardGrid = ({ ...restProps }: DC.BoardGridProps) => {
 export const PureBoardGrid = ({ ...restProps }: DC.PureBoardGridProps) => {
   const locale = window.localStorage.getItem('locale') || 'zh';
   setLocale(locale);
-  return <DCPureBoardGrid {...restProps} />;
+  const _restProps = produce(restProps, draft => {
+    forEach(draft.layout, item => {
+      item.view.api.url = setApiWithOrg(item.view.api.url);
+    });
+  });
+
+  return <DCPureBoardGrid {..._restProps} />;
 };
 
