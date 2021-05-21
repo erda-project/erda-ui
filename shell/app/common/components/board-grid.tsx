15bd3706936a28168c8f4b8caff4de04c0ea862b
zxj <782803291@qq.com>
2021-05-21 17:42:20 +0800

    fix: fix board grid api url (#230)

--- shell/app/common/components/board-grid.tsx
+++ shell/app/common/components/board-grid.tsx
@@ -12,15 +12,12 @@
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import * as React from 'react';
-import { setApiWithOrg } from 'common/utils';
 import {
   BoardGrid as DCBoardGrid,
   PureBoardGrid as DCPureBoardGrid,
   setLocale,
   DC,
 } from '@terminus/dashboard-configurator';
-import produce from 'immer';
-import { forEach } from 'lodash';
 
 export const BoardGrid = ({ ...restProps }: DC.BoardGridProps) => {
   const locale = window.localStorage.getItem('locale') || 'zh';
@@ -31,14 +28,6 @@ export const BoardGrid = ({ ...restProps }: DC.BoardGridProps) => {
 export const PureBoardGrid = ({ ...restProps }: DC.PureBoardGridProps) => {
   const locale = window.localStorage.getItem('locale') || 'zh';
   setLocale(locale);
-  const _restProps = produce(restProps, draft => {
-    forEach(draft.layout, item => {
-      if (item.view.api) {
-        item.view.api.url = setApiWithOrg(item.view.api.url);
-      }
-    });
-  });
-
-  return <DCPureBoardGrid {..._restProps} />;
+  return <DCPureBoardGrid {...restProps} />;
 };
 
