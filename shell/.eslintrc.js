a129d020358a95642bf3c82e7f80619c9a26f91f
陈韡韬 <mcdaddychen@126.com>
2021-06-07 16:23:15 +0800

    update shell eslint setup

--- shell/.eslintrc.js
+++ shell/.eslintrc.js
@@ -2,44 +2,29 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
-    node: true,
   },
   globals: {
-    Cypress: "readonly",
-    cy: "readonly",
+    Cypress: 'readonly',
+    cy: 'readonly',
   },
   extends: [
     'eslint-config-ali/typescript/react',
-    'eslint-config-ali/typescript'
   ],
-  parser: 'babel-eslint',
-  plugins: [
-    'eslint-plugin-react',
-    'eslint-plugin-react-hooks',
-    'import',
-    'compat',
-    'jsx-a11y',
-  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   parserOptions: {
-    ecmaVersion: 2015, // specify the version of ECMAScript syntax you want to use: 2015 => (ES6)
-    sourceType: 'module',  // Allows for the use of imports
+    ecmaVersion: 2020, // specify the version of ECMAScript syntax you want to use: 2015 => (ES6)
+    sourceType: 'module', // Allows for the use of imports
     ecmaFeatures: {
       jsx: true, // enable JSX
-      impliedStrict: true // enable global strict mode
-    }
-  },
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx'],
-        moduleDirectory: ['node_modules', 'app', 'router', 'router/modules']
-      },
-    }
+      impliedStrict: true, // enable global strict mode
+    },
+    project: ['./tsconfig.json'], // Specify it only for TypeScript files
+    tsconfigRootDir: __dirname,
   },
   rules: {
     'no-param-reassign': ['error', { props: true, ignorePropertyModificationsFor: ['draft', 'state'] }],
     'import/prefer-default-export': 'off',
-    // 'react/jsx-filename-extension': [2, { 'extensions': ['.js', '.jsx', '.ts', '.tsx'] }],
     'react/prop-types': 'off',
     'arrow-body-style': 'off',
     'max-len': 'off',
@@ -51,30 +36,21 @@ module.exports = {
     'import/no-named-as-default-member': 'off',
     'react-hooks/rules-of-hooks': 'error',
     'no-console': 2,
-    '@typescript-eslint/ban-ts-ignore': 'off'
+    '@typescript-eslint/ban-ts-ignore': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': [1, { argsIgnorePattern: '^_', varsIgnorePattern: '^ignored?$' }],
+    '@typescript-eslint/interface-name-prefix': 'off',
+    indent: 0,
   },
   overrides: [
-    {
-      files: ['**/*.tsx', '**/*.ts'],
-      parser: '@typescript-eslint/parser',
-      plugins: [
-        '@typescript-eslint'
-      ],
-      rules: {
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        '@typescript-eslint/no-explicit-any': 'off',
-        "@typescript-eslint/no-unused-vars": [1, { "argsIgnorePattern": "^_", "varsIgnorePattern": "^ignored?$" }],
-        '@typescript-eslint/interface-name-prefix': 'off',
-        'indent': 0,
-      }
-    },
     {
       files: ['**/*.js'], // none ts scripts, like webpack config or legacy node scripts
       rules: {
         '@typescript-eslint/no-var-requires': 'off',
         '@typescript-eslint/no-require-imports': 'off',
-        'no-console': 'off'
-      }
-    }
-  ]
+        'no-console': 'off',
+      },
+    },
+  ],
 };
