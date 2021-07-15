79a53f0c23549fc8377263e71276625fe840a089
老俊 <782803291@qq.com>
2021-07-15 16:47:55 +0800

    fix: fix dice yaml editor form error

--- shell/app/modules/application/common/components/health-check-field.tsx
+++ shell/app/modules/application/common/components/health-check-field.tsx
@@ -11,188 +11,176 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { Input, InputNumber, Select } from 'app/nusi';
+import { useUpdate } from 'common';
 import i18n from 'i18n';
 import './variable-input-group.scss';
 
 const { Option } = Select;
 
 interface IVariableInputGroupProps {
-  value: any;
-  placeholder?: string;
+  value: Obj;
   disabled?: boolean;
-  onChange: (options: any[]) => void;
+  onChange: (val: Obj) => void;
 }
 
 const defaultValue = {
   healthCheckKey: null,
 };
 
-export default class extends PureComponent<IVariableInputGroupProps, any> {
-  constructor(props: IVariableInputGroupProps) {
-    super(props);
-    const keys = Object.keys(props.value);
-    let healthCheckKey;
-    if (keys.length) {
-      healthCheckKey = keys[0];
+const HealthCheckField = (props: IVariableInputGroupProps) => {
+  const { value: propsValue, disabled, onChange } = props;
+  const [{ healthCheckKey, value }, updater, update] = useUpdate({
+    healthCheckKey: Object.keys(propsValue)?.[0],
+    value: propsValue || defaultValue,
+  });
+
+  React.useEffect(() => {
+    update({
+      healthCheckKey: Object.keys(propsValue)?.[0],
+      value: propsValue,
+    });
+  }, [propsValue, update]);
+
+  const triggerChange = (changedValue: Obj) => {
+    onChange?.(changedValue);
+  };
+
+  const changeType = (v: string) => {
+    triggerChange({
+      [v]: {},
+    });
+  };
+
+  const changeValue = (v: string | number | undefined, key: string) => {
+    let _value = { ...value };
+    if (!_value[healthCheckKey]) {
+      _value = {
+        [healthCheckKey]: {},
+      };
     }
-    this.state = {
-      healthCheckKey,
-      value: props.value || defaultValue,
-    };
+    _value[healthCheckKey][key] = v;
+    triggerChange(_value);
+  };
+
+  let Comp = null;
+  switch (healthCheckKey) {
+    case 'http':
+      Comp = <HttpComp disabled={disabled} value={value?.[healthCheckKey]} changeValue={changeValue} />;
+      break;
+    case 'exec':
+      Comp = <CommandComp disabled={disabled} value={value?.[healthCheckKey]} changeValue={changeValue} />;
+      break;
+    default:
+      break;
   }
-
-  static getDerivedStateFromProps(nextProps: IVariableInputGroupProps) {
-    const keys = Object.keys(nextProps.value);
-    let healthCheckKey;
-    if (keys.length) {
-      healthCheckKey = keys[0];
-    }
-    return {
-      healthCheckKey,
-      value: nextProps.value || defaultValue,
-    };
-  }
-
-  triggerChange = (changedValue: any) => {
-    const { onChange } = this.props;
-    if (onChange) {
-      onChange(changedValue);
-    }
-  };
-
-  render() {
-    const { disabled } = this.props;
-    const { healthCheckKey } = this.state;
-
-    let content = null;
-    switch (healthCheckKey) {
-      case 'http':
-        content = this.renderHttp();
-        break;
-      case 'exec':
-        content = this.renderCommand();
-        break;
-      default:
-        break;
-    }
-    return (
+  return (
+    <div>
       <div>
-        <div>
-          <span className="edit-service-label">{i18n.t('application:health check mechanism')}: </span>
-          <span>
-            <Select disabled={disabled} value={healthCheckKey} onChange={(e: any) => this.changeValue(e, 'key')}>
-              <Option value="http">HTTP</Option>
-              <Option value="exec">COMMAND</Option>
-            </Select>
-          </span>
-        </div>
-        {content}
+        <span className="edit-service-label">{i18n.t('application:health check mechanism')}: </span>
+        <span>
+          <Select disabled={disabled} value={healthCheckKey} onChange={(e: string) => changeType(e)}>
+            <Option value="http">HTTP</Option>
+            <Option value="exec">COMMAND</Option>
+          </Select>
+        </span>
       </div>
-    );
-  }
+      {Comp}
+    </div>
+  );
+};
 
-  private renderHttp = () => {
-    const { disabled } = this.props;
-    const { value, healthCheckKey } = this.state;
-    return (
-      <div>
-        <div>
-          <span className="edit-service-label">{i18n.t('application:port')}: </span>
-          <span>
-            <InputNumber
-              disabled={disabled}
-              min={1}
-              className="full-width"
-              value={value[healthCheckKey] ? value[healthCheckKey].port : null}
-              onChange={(v?: number) => this.changeValue(v, 'port')}
-              placeholder={i18n.t('application:please enter the port')}
-            />
-          </span>
-        </div>
-        <div>
-          <span className="edit-service-label">URI {i18n.t('application:path')}: </span>
-          <span>
-            <Input
-              disabled={disabled}
-              value={value[healthCheckKey] ? value[healthCheckKey].path : null}
-              onChange={(e: any) => this.changeValue(e.target.value, 'path')}
-              placeholder={i18n.t('application:please enter the path')}
-            />
-          </span>
-        </div>
-        <div>
-          <span className="edit-service-label">{i18n.t('application:duration')}: </span>
-          <span>
-            <InputNumber
-              disabled={disabled}
-              className="full-width"
-              value={value[healthCheckKey] ? value[healthCheckKey].duration || 0 : 0}
-              onChange={(v?: number) => this.changeValue(v, 'duration')}
-              placeholder={i18n.t('application:please enter the duration')}
-              min={1}
-              formatter={(v: any) => `${v}秒`}
-              parser={(v: any) => v.replace('秒', '')}
-            />
-          </span>
-        </div>
-      </div>
-    );
-  };
-
-  private renderCommand = () => {
-    const { disabled } = this.props;
-    const { value, healthCheckKey } = this.state;
-    return (
-      <div>
-        <div>
-          <span className="edit-service-label">{i18n.t('application:command')}: </span>
-          <span>
-            <Input
-              disabled={disabled}
-              value={value[healthCheckKey] ? value[healthCheckKey].cmd : null}
-              onChange={(e: any) => this.changeValue(e.target.value, 'cmd')}
-              placeholder={i18n.t('application:please enter the command')}
-            />
-          </span>
-        </div>
-        <div>
-          <span className="edit-service-label">{i18n.t('application:duration')}: </span>
-          <span>
-            <InputNumber
-              disabled={disabled}
-              className="full-width"
-              value={value[healthCheckKey] ? value[healthCheckKey].duration || 0 : null}
-              onChange={(v?: number) => this.changeValue(v, 'duration')}
-              placeholder={i18n.t('application:please enter the duration')}
-              min={1}
-              formatter={(v: any) => `${v}秒`}
-              parser={(v: any) => v.replace('秒', '')}
-            />
-          </span>
-        </div>
-      </div>
-    );
-  };
-
-  private changeValue = (v: any, key: string) => {
-    const { value } = this.state;
-    let { healthCheckKey } = this.state;
-    if (key === 'key') {
-      healthCheckKey = v;
-      value[healthCheckKey] = {};
-    } else {
-      if (!value[healthCheckKey]) {
-        value[healthCheckKey] = {};
-      }
-      // @ts-ignore
-      value[healthCheckKey][key] = v;
-    }
-    const state = {
-      healthCheckKey,
-      value,
-    };
-    this.triggerChange(state.value);
-  };
+interface IProps {
+  disabled?: boolean;
+  value?: { path: string; port: number; duration: number };
+  changeValue: (v: string | number | undefined, key: string) => void;
 }
+
+const HttpComp = (props: IProps) => {
+  const { disabled, value, changeValue } = props;
+  return (
+    <div>
+      <div>
+        <span className="edit-service-label">{i18n.t('application:port')}: </span>
+        <span>
+          <InputNumber
+            disabled={disabled}
+            min={1}
+            className="full-width"
+            value={value?.port}
+            onChange={(v?: number) => changeValue(v, 'port')}
+            placeholder={i18n.t('application:please enter the port')}
+          />
+        </span>
+      </div>
+      <div>
+        <span className="edit-service-label">URI {i18n.t('application:path')}: </span>
+        <span>
+          <Input
+            disabled={disabled}
+            value={value?.path}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => changeValue(e.target.value, 'path')}
+            placeholder={i18n.t('application:please enter the path')}
+          />
+        </span>
+      </div>
+      <div>
+        <span className="edit-service-label">{i18n.t('application:duration')}: </span>
+        <span>
+          <InputNumber
+            disabled={disabled}
+            className="full-width"
+            value={value?.duration || 0}
+            onChange={(v?: number) => changeValue(v, 'duration')}
+            placeholder={i18n.t('application:please enter the duration')}
+            min={1}
+            formatter={(v: any) => `${v}秒`}
+            parser={(v: any) => v.replace('秒', '')}
+          />
+        </span>
+      </div>
+    </div>
+  );
+};
+
+interface ICommandProps {
+  disabled?: boolean;
+  value?: { cmd: string; duration: number };
+  changeValue: (v: string | number | undefined, key: string) => void;
+}
+const CommandComp = (props: ICommandProps) => {
+  const { disabled, value, changeValue } = props;
+  return (
+    <div>
+      <div>
+        <span className="edit-service-label">{i18n.t('application:command')}: </span>
+        <span>
+          <Input
+            disabled={disabled}
+            value={value?.cmd}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => changeValue(e.target.value, 'cmd')}
+            placeholder={i18n.t('application:please enter the command')}
+          />
+        </span>
+      </div>
+      <div>
+        <span className="edit-service-label">{i18n.t('application:duration')}: </span>
+        <span>
+          <InputNumber
+            disabled={disabled}
+            className="full-width"
+            value={value?.duration || 0}
+            onChange={(v?: number) => changeValue(v, 'duration')}
+            placeholder={i18n.t('application:please enter the duration')}
+            min={1}
+            formatter={(v: any) => `${v}秒`}
+            parser={(v: any) => v.replace('秒', '')}
+          />
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default HealthCheckField;
