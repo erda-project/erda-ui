46cd085e011b1831db7195c6a6494615a420805c
zxj <782803291@qq.com>
2021-07-16 10:45:53 +0800

    fix: fix tooltip undefined (#666)

--- shell/app/modules/dcos/pages/service-manager/rds-manager/rds-account.tsx
+++ shell/app/modules/dcos/pages/service-manager/rds-manager/rds-account.tsx
@@ -12,7 +12,7 @@
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import * as React from 'react';
-import { Table, Button } from 'app/nusi';
+import { Table, Button, Tooltip } from 'app/nusi';
 import { useUpdate } from 'common';
 import { map } from 'lodash';
 import i18n from 'i18n';
