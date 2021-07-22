f0b3a81488b4902efccb7e2dbd1a3a56474f38bf
戣蓦 <mcdaddychen@126.com>
2021-07-22 09:39:30 +0800

    copywriting issue & command drawer shown when creating edas issue (#726)
    * fix(cmp): copywriting issue
    
    * fix(cmp): add show commad drawer condition

--- shell/app/modules/cmp/pages/cluster-manage/cluster-list.tsx
+++ shell/app/modules/cmp/pages/cluster-manage/cluster-list.tsx
@@ -304,7 +304,7 @@ const ClusterList = ({ dataSource, onEdit }: IProps) => {
       title: i18n.t('default:description'),
       dataIndex: 'description',
       ellipsis: true,
-      render: (text) => text || '_',
+      render: (text) => text || '-',
     },
     {
       title: i18n.t('application:type'),
