2e6985ba16c181cbc17bea5e720f77936076b408
sherotree <wytreeshero@163.com>
2021-08-06 00:22:59 +0800

    refactor: remove duplicate EmptyHolder

--- shell/app/modules/project/pages/data-source/index.tsx
+++ shell/app/modules/project/pages/data-source/index.tsx
@@ -126,7 +126,6 @@ export default function DataSourceManagement() {
 
   return (
     <>
-      <EmptyHolder relative />
       {addonList ? (
         <AddonCardList
           isFetching={loadingAddons}
