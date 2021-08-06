dc728b33895eedcacedcf19ef92117e06ed21831
hujiahao-hjh <82502479+hujiahao-hjh@users.noreply.github.com>
2021-08-06 17:23:32 +0800

    refactor: replace Nusi Card with Antd (#874)
    * refactor: replace Nusi Card with Antd
    
    * refactor: code optimization

--- shell/app/modules/msp/monitor/trace-insight/pages/trace-querier/trace-common-panel.tsx
+++ shell/app/modules/msp/monitor/trace-insight/pages/trace-querier/trace-common-panel.tsx
@@ -27,9 +27,9 @@ const TraceCommonPanel = (props: IProps) => {
 
   return (
     <Card className={`${className} trace-common-panel`} bordered={false} {...otherProps}>
-      <Card.Header
-        title={isString(title) ? <h3 className="trace-common-panel-title font-medium">{title}</h3> : title || null}
-      />
+      <div className="mb-2">
+        {isString(title) ? <h3 className="trace-common-panel-title font-medium">{title}</h3> : title || null}
+      </div>
       <div className="card-body">{children}</div>
     </Card>
   );
