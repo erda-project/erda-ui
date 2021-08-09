e0ae1455e96e8eef89acb2ac5dbce74aff3da789
hujiahao-hjh <82502479+hujiahao-hjh@users.noreply.github.com>
2021-08-09 19:17:19 +0800

    fix: trace debug add params style bug (#857)
    * fix: trace debug add params style bug
    
    * fix: use official interaction
    
    * fix: use official interaction
    
    * fix: code optimization

--- shell/app/common/components/key-value-table.tsx
+++ shell/app/common/components/key-value-table.tsx
@@ -269,6 +269,7 @@ export class KeyValueTable extends React.Component<IProps, IState> {
         title: 'KEY',
         dataIndex: 'key',
         width: 280,
+        ellipsis: false,
         render: (text: string, record: IItemData) => (
           <InputItem
             form={form}
@@ -288,6 +289,7 @@ export class KeyValueTable extends React.Component<IProps, IState> {
       {
         title: 'VALUE',
         dataIndex: 'value',
+        ellipsis: false,
         render: (text: string, record: IItemData) => (
           <InputItem
             form={form}
@@ -345,7 +347,7 @@ export class KeyValueTable extends React.Component<IProps, IState> {
           ref={(ref) => {
             this.table = ref;
           }}
-          scroll={{ x: 800 }}
+          scroll={undefined}
         />
       </div>
     );
