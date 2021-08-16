171a066e2b325a12aa8fcb30694f97a353ce5237
RayZhangzhang <16114124@hdu.edu.cn>
2021-08-16 22:49:44 +0800

    test(common): add auto ui test

--- shell/auto_test/tests/dop/merge-requests.spec.ts
+++ shell/auto_test/tests/dop/merge-requests.spec.ts
@@ -13,36 +13,45 @@
 
 import { Role, test, expect } from '../../fixtures';
 
+const allRequestUrl = 'https://erda.hkci.terminus.io/integration/dop/projects/123/apps/788/repo/mr/open';
+const createMRUrl = 'https://erda.hkci.terminus.io/integration/dop/projects/123/apps/788/repo/mr/open/createMR';
+
+const createRequest = async (page, wait, expectExist, title) => {
+  await page.click('text=new merge request');
+  await wait(1);
+  expect(page.url()).toBe(createMRUrl);
+
+  await page.click('.repo-branch-select:has-text("compare:")');
+  expectExist('div[role="tooltip"]', 1);
+  await page.click('div[role="tooltip"] >> li:has-text("hotfix/do-not-delete")');
+  expectExist('text=compare:hotfix/do-not-delete', 1);
+
+  await page.click('.repo-mr-form >> input[type="text"]');
+  await page.fill('.repo-mr-form >> input[type="text"]', title);
+  await page.click('.repo-mr-form >> textarea[name="textarea"]');
+  await page.fill('.repo-mr-form >> textarea[name="textarea"]', 'description');
+  await page.click('.repo-mr-form >> text=choose self');
+  expectExist('.repo-mr-form >> img[alt="e [20x20]"]', 1);
+  await page.click('button:has-text("submit")');
+  await wait(1);
+
+  expect(page.url()).toBe(allRequestUrl);
+  expectExist(`text=${title}`, 1);
+};
+
 Role('Manager', () => {
   test('merge requests', async ({ page, expectExist, wait, expectRequestSuccess }) => {
     await expectRequestSuccess();
-    await page.goto('https://erda.hkci.terminus.io/integration/dop/projects/123/apps/788/repo/mr/open');
-    await wait(1);
 
-    await page.click('text=new merge request');
+    await page.goto(allRequestUrl);
     await wait(1);
-    expect(page.url()).toBe(
-      'https://erda.hkci.terminus.io/integration/dop/projects/123/apps/788/repo/mr/open/createMR',
-    );
-
-    await page.click('.repo-branch-select:has-text("compare:")');
-    await wait(1);
-    expectExist('div[role="tooltip"]', 1);
-    await page.click('div[role="tooltip"] >> li:has-text("hotfix/do-not-delete")');
-    expectExist('text=compare:hotfix/do-not-delete', 1);
 
     const now = Date.now();
-    await page.click('.repo-mr-form >> input[type="text"]');
-    await page.fill('.repo-mr-form >> input[type="text"]', `title${now}`);
-    await page.click('.repo-mr-form >> textarea[name="textarea"]');
-    await page.fill('.repo-mr-form >> textarea[name="textarea"]', 'description');
-    await page.click('.repo-mr-form >> text=choose self');
-    expectExist('.repo-mr-form >> img[alt="e [20x20]"]', 1);
-    await page.click('button:has-text("submit")');
-    await wait(1);
-    expect(page.url()).toBe('https://erda.hkci.terminus.io/integration/dop/projects/123/apps/788/repo/mr/open');
-    expectExist(`text=title${now}`, 1);
-    await page.click(`text=title${now}`);
+
+    await createRequest(page, wait, expectExist, `firstTitle${now}`);
+    await createRequest(page, wait, expectExist, `secondTitle${now}`);
+
+    await page.click(`text=firstTitle${now}`);
     await wait(1);
     expectExist('.ant-tabs-tab-active:has-text("comment")', 1);
     await page.click('textarea[name="textarea"]');
@@ -61,13 +70,25 @@ Role('Manager', () => {
 
     await page.click('button:has-text("close")');
     await wait(1);
-    expect(page.url()).toBe('https://erda.hkci.terminus.io/integration/dop/projects/123/apps/788/repo/mr/open');
+    expect(page.url()).toBe(allRequestUrl);
     await page.click('.tab-menu-item >> text=closed');
     await wait(1);
     expect(page.url()).toBe('https://erda.hkci.terminus.io/integration/dop/projects/123/apps/788/repo/mr/closed');
-    expectExist(`text=title${now}`, 1);
-    await page.click(`text=title${now}`);
+    expectExist(`text=firstTitle${now}`, 1);
+    await page.click(`text=firstTitle${now}`);
     await wait(1);
     expectExist('text=merge request detail', 1);
+  });
+});
+
+Role('Dev', () => {
+  test.only('can not edit or merge request', async ({ page, expectExist, expectRequestSuccess, wait }) => {
+    await expectRequestSuccess();
+    await page.goto(allRequestUrl);
+    await wait(1);
+    await page.click(`:text-matches("secondTitle")`);
+    await wait(1);
+    expectExist('.disabled >> button:has-text("edit")', 1);
+    expectExist('.disabled >> button:has-text("merge")', 1);
   });
 });
