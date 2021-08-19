63336055b7629fd26fae11aad8b2569edac3e885
Ray <48085754+Rayzhangzhang@users.noreply.github.com>
2021-08-19 10:19:27 +0800

    add UI test (#978)
    * test(common): add auto ui test
    
    * test(common): add auto ui test
    
    * test(common): add auto ui test
    
    * test(common): add ui auto test
    
    * test(common): add ui auto test

--- shell/auto_test/fixtures.ts
+++ shell/auto_test/fixtures.ts
@@ -39,6 +39,9 @@ const gotoMap = {
   orgProjectList: '/integration/orgCenter/projects',
   orgCreateProject: '/integration/orgCenter/projects/createProject',
   pipeline: '/integration/dop/projects/123/apps/788/pipeline',
+  branches: '/integration/dop/projects/123/apps/788/repo/branches',
+  mergeRequest: '/integration/dop/projects/123/apps/788/repo/mr/open',
+  branchDevelop: '/integration/dop/projects/123/apps/788/repo/tree/develop',
 };
 
 // Extend base test with our fixtures.
