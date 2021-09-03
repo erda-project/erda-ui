c4f11ede5f9b8dbe5b3111518da301e2cafc42d9
老俊 <782803291@qq.com>
2021-09-03 11:33:02 +0800

    fix: delete useless

--- shell/app/common/components/tiled-filter.tsx
+++ shell/app/common/components/tiled-filter.tsx
@@ -11,7 +11,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-import * as React from 'react';
+import React from 'react';
 import { Input } from 'core/nusi';
 import { Search as IconSearch, Down as IconDown } from '@icon-park/react';
 import i18n from 'i18n';
