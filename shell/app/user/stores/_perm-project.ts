65945664f57b47f551889885cda24a721ef94c21
Zero <mobius_pan@yeah.net>
2021-09-06 16:07:53 +0800

    feat(msp): add permission page i18n

--- shell/app/user/stores/_perm-project.ts
+++ shell/app/user/stores/_perm-project.ts
@@ -13,7 +13,6 @@
 import i18n from 'i18n';
 
 // 通过权限配置页面导出角色数据覆盖，勿手动修改
-import i18n from 'i18n';
 
 export const projectRoleMap = {
   Owner: { name: i18n.t('user:Project Owner'), value: 'Owner' },
@@ -525,19 +524,19 @@ export const projectPerm = {
       },
     },
     accessConfiguration: {
-      name: '接入配置',
+      name: i18n.t('msp:access configuration'),
       createAccessKey: {
-        name: '创建 AccessKey',
+        name: i18n.t('create {name}', { name: 'AccessKey' }),
         pass: false,
         role: ['Owner', 'Lead'],
       },
       deleteAccessKey: {
-        name: '删除 AccessKey',
+        name: i18n.t('delete {name}', { name: 'AccessKey' }),
         pass: false,
         role: ['Owner', 'Lead'],
       },
       viewAccessKeySecret: {
-        name: '查看 AccessKeySecret',
+        name: i18n.t('view {name}', { name: 'AccessKeySecret' }),
         pass: false,
         role: ['Owner', 'Lead'],
       },
