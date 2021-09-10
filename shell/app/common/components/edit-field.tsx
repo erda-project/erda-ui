8632bba20c23b4202120adf854f176041ebf5e88
随风 <daskyrk@users.noreply.github.com>
2021-09-10 23:01:12 +0800

    fix(dop): discard issue edit change not work (#1159)

--- shell/app/common/components/edit-field.tsx
+++ shell/app/common/components/edit-field.tsx
@@ -196,7 +196,7 @@ export const EditField = React.forwardRef((props: IProps, _compRef) => {
           {...itemProps}
           value={editValue}
           onChange={updater.editValue}
-          onSave={(v) => onChangeCb?.({ [name]: v })}
+          onSave={(v) => onChangeCb?.({ [name]: v }, 'markdown')}
           originalValue={originalValue}
           disabled={disabled}
         />
