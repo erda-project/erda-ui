b32086adda03b5042fc80c15c0200160ca04b15f
老俊 <782803291@qq.com>
2021-09-14 17:04:35 +0800

    fix: delete useless

--- shell/app/modules/dop/router.js
+++ shell/app/modules/dop/router.js
@@ -179,7 +179,6 @@ export default function getDopRouter() {
           path: 'mock',
           pageName: '动态界面测试',
           // layout: { showSubSidebar: false, fullHeight: true },
-          layout: { noWrapper: true },
           getComp: (cb) => cb(import('app/config-page/mock')),
         },
         // UI_ENV exist and not PROD, then open the debug page
