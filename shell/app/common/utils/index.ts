aa632a8af59319ce8364251e041c541733b2fb08
wuhuiluo <48612739+wuhuiluo@users.noreply.github.com>
2021-09-22 22:08:37 +0800

    feat(msp): Microservice item details modification (#1315)
    * feat(msp): modify the text micro service governance platform
    
    * feat(msp): modify the translation problem and modify the text to global topology

--- shell/app/common/utils/index.ts
+++ shell/app/common/utils/index.ts
@@ -143,7 +143,7 @@ export const regRules = {
   banFullWidthPunctuation: {
     pattern:
       /^[^\u3002|\uff1f|\uff01|\uff0c|\u3001|\uff1b|\uff1a|\u201c|\u201d|\u2018|\u2019|\uff08|\uff09|\u300a|\u300b|\u3008|\u3009|\u3010|\u3011|\u300e|\u300f|\u300c|\u300d|\ufe43|\ufe44|\u3014|\u3015|\u2026|\u2014|\uff5e|\ufe4f|\uffe5]+$/,
-    message: i18n.t('project:do not allow w-full punctuation'),
+    message: i18n.t('project:full-width punctuation is not allowed"'),
   },
   dingding: {
     pattern: /^https:\/\/oapi\.dingtalk\.com\//g,
