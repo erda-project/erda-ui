4bd556092295f72ac6b710270bfe3085f1b572fd
老俊 <782803291@qq.com>
2021-10-14 14:34:15 +0800

    fix: update menu in orgCenter

--- shell/app/modules/org/pages/projects/project-list.tsx
+++ shell/app/modules/org/pages/projects/project-list.tsx
@@ -16,113 +16,96 @@ import i18n from 'i18n';
 import { Table, Spin, Button, Tooltip } from 'core/nusi';
 import { ColumnProps } from 'core/common/interface';
 import { goTo, fromNow } from 'common/utils';
-import { useEffectOnce } from 'react-use';
-import { SearchTable, Icon as CustomIcon } from 'common';
+import { useUnmount } from 'react-use';
+import { ChartHistogramTwo as IconChartHistogramTwo } from '@icon-park/react';
+import { SearchTable } from 'common';
+import { PAGINATION } from 'app/constants';
 import projectStore from 'project/stores/project';
 import { useLoading } from 'core/stores/loading';
 import './project-list.scss';
 
-interface IBoxProp {
-  data: Array<[string | number, string]>;
+interface IState {
+  pageNo: number;
+  pageSize?: number;
+  query?: string;
+  orderBy?: string;
+  asc?: boolean;
 }
-const SplitBox = ({ data }: IBoxProp) => {
-  return (
-    <div className="inline-split-box">
-      {data.map((item, i) => {
-        return (
-          <div key={String(i)} className="item">
-            <div className="count">{item[0]}</div>
-            <div className="text-xs">{item[1]}</div>
-          </div>
-        );
-      })}
-    </div>
-  );
-};
 export const ProjectList = () => {
   const [list, paging] = projectStore.useStore((s) => [s.list, s.paging]);
   const { getProjectList } = projectStore.effects;
   const { clearProjectList } = projectStore.reducers;
   const { pageNo, pageSize, total } = paging;
   const [loadingList] = useLoading(projectStore, ['getProjectList']);
-
-  useEffectOnce(() => {
-    getProjectList({ pageNo: 1, orderBy: 'activeTime', asc: false });
-    return () => {
-      clearProjectList();
-    };
+  const [searchObj, setSearchObj] = React.useState<IState>({
+    pageNo: 1,
+    pageSize,
+    query: '',
+    orderBy: 'activeTime',
+    asc: false,
   });
+
+  useUnmount(() => {
+    clearProjectList();
+  });
+
+  const getColumnOrder = (key?: string) => {
+    if (key) {
+      return searchObj.orderBy === key ? (searchObj.asc ? 'ascend' : 'descend') : undefined;
+    }
+    return undefined;
+  };
 
   const getColumns = () => {
     const columns: Array<ColumnProps<PROJECT.Detail>> = [
       {
+        title: i18n.t('project:project ID'),
+        dataIndex: 'id',
+        key: 'id',
+        width: 80,
+      },
+      {
         title: i18n.t('project name'),
         dataIndex: 'displayName',
         key: 'displayName',
-        fixed: 'left',
-        width: 160,
+        width: 200,
         ellipsis: {
           showTitle: false,
         },
         render: (text) => <Tooltip title={text}>{text}</Tooltip>,
       },
       {
-        title: i18n.t('msp:project type'),
-        dataIndex: 'type',
-        key: 'type',
-        width: 160,
-        render: (text: string) => (text === 'MSP' ? i18n.t('org:microservice Observation Project') : 'DevOps'),
-      },
-      {
         title: i18n.t('org:application/Member Statistics'),
         dataIndex: 'stats',
         key: 'countApplications',
-        width: 160,
-        render: (stats: PROJECT.ProjectStats) => (
-          <SplitBox
-            data={[
-              [stats.countApplications, i18n.t('application')],
-              [stats.countMembers, i18n.t('org:member')],
-            ]}
-          />
-        ),
-      },
-      {
-        title: <div className="text-center">{i18n.t('org:iteration statistics')}</div>,
-        dataIndex: 'stats',
-        key: 'iteration',
-        width: 200,
-        render: (stats: PROJECT.ProjectStats) => (
-          <SplitBox
-            data={[
-              [stats.totalIterationsCount, i18n.t('org:total')],
-              [stats.runningIterationsCount, i18n.t('org:processing')],
-              [stats.planningIterationsCount, i18n.t('org:plan')],
-            ]}
-          />
-        ),
-      },
-      {
-        title: <div className="text-center">{`${i18n.t('org:workload')}(${i18n.t('man-day')})`}</div>,
-        dataIndex: 'stats',
-        key: 'work',
-        width: 200,
-        render: (stats: PROJECT.ProjectStats) => (
-          <SplitBox
-            data={[
-              [stats.totalManHourCount, i18n.t('org:total')],
-              [stats.usedManHourCount, i18n.t('org:logged')],
-              [stats.planningManHourCount, i18n.t('org:planning')],
-            ]}
-          />
-        ),
-      },
-      {
-        title: i18n.t('org:defect resolution rate'),
-        dataIndex: 'stats',
-        key: 'bug',
         width: 120,
-        render: (stats: PROJECT.ProjectStats) => `${stats.doneBugPercent}%`,
+        render: (stats: PROJECT.ProjectStats) => `${stats.countApplications} / ${stats.countMembers}`,
+      },
+      {
+        title: i18n.t('total CPU allocation'),
+        dataIndex: 'cpuQuota',
+        key: 'cpuQuota',
+        width: 120,
+        sorter: true,
+        sortOrder: getColumnOrder('cpuQuota'),
+        render: (text: string) => `${text} Core`,
+      },
+      {
+        title: i18n.t('total Memory allocation'),
+        dataIndex: 'memQuota',
+        key: 'memQuota',
+        width: 120,
+        sorter: true,
+        sortOrder: getColumnOrder('memQuota'),
+        render: (text: string) => `${text} GiB`,
+      },
+
+      {
+        title: i18n.t('msp:project type'),
+        dataIndex: 'type',
+        key: 'type',
+        width: 120,
+        render: (text: string) => (text === 'MSP' ? i18n.t('org:microservice Observation Project') : 'DevOps'),
       },
       {
         title: i18n.t('latest active'),
@@ -130,121 +113,15 @@ export const ProjectList = () => {
         key: 'activeTime',
         width: 120,
         sorter: true,
+        sortOrder: getColumnOrder('activeTime'),
         render: (text) => (text ? fromNow(text) : i18n.t('none')),
       },
-      {
-        title: i18n.t('total CPU allocation'),
-        dataIndex: 'cpuQuota',
-        key: 'cpuQuota',
-        width: 200,
-        sorter: true,
-        render: (text: string) => `${text} Core`,
-      },
-      {
-        title: i18n.t('total Memory allocation'),
-        dataIndex: 'memQuota',
-        key: 'memQuota',
-        width: 200,
-        sorter: true,
-        render: (text: string) => `${text} GiB`,
-      },
-      {
-        title: <span>CPU / {i18n.t('Memory usage')}</span>,
-        dataIndex: 'usage',
-        key: 'usage',
-        render: (t, record: PROJECT.Detail) => {
-          const {
-            cpuServiceUsed = 0,
-            memServiceUsed = 0,
-            cpuAddonUsed = 0,
-            memAddonUsed = 0,
-            memQuota,
-            cpuQuota,
-          } = record;
 
-          const renderBar = (serviceUsed: number, addonUsed: number, totalNum: number, unit: string) => {
-            const isOveruse = serviceUsed + addonUsed > totalNum;
-            const leftNum = totalNum - (serviceUsed + addonUsed);
-            const percent = isOveruse
-              ? [
-                  // 超过总量
-                  Math.floor((serviceUsed / (serviceUsed + addonUsed)) * 100),
-                  Math.floor((addonUsed / (serviceUsed + addonUsed)) * 100),
-                  0,
-                ]
-              : totalNum === 0
-              ? [
-                  // 总量、使用量都为0
-                  0, 0, 100,
-                ]
-              : [
-                  Math.round((serviceUsed / totalNum) * 100),
-                  Math.round((addonUsed / totalNum) * 100),
-                  Math.abs(100 - Math.round((serviceUsed / totalNum) * 100) - Math.round((addonUsed / totalNum) * 100)),
-                ];
-            const percentText = percent.map((item) => `${item}%`);
-
-            return (
-              <div className={`quota-container ${isOveruse ? 'overuse' : ''}`}>
-                <div className={'quota-bar'}>
-                  <Tooltip
-                    title={`${i18n.t('cmp:application used')} ${serviceUsed.toFixed(2)}${unit} (${
-                      isOveruse ? `${Math.floor(totalNum ? (serviceUsed / totalNum) * 100 : 100)}%` : percentText[0]
-                    })`}
-                  >
-                    <div
-                      className={`nowrap ${percentText[0] !== '0%' ? 'border-right-color' : ''}`}
-                      style={{ width: percentText[0] }}
-                    >
-                      {i18n.t('project:application')}
-                    </div>
-                  </Tooltip>
-                  <Tooltip
-                    title={`${i18n.t('cmp:addon used')} ${addonUsed.toFixed(2)}${unit} (${
-                      isOveruse ? `${Math.floor(totalNum ? (addonUsed / totalNum) * 100 : 100)}%` : percentText[1]
-                    })`}
-                  >
-                    <div
-                      className={`nowrap ${percentText[1] !== '0%' ? 'border-right-color' : ''}`}
-                      style={{ width: percentText[1] }}
-                    >
-                      addon
-                    </div>
-                  </Tooltip>
-                  <Tooltip
-                    title={`${i18n.t('available')} ${leftNum <= 0 ? 0 : leftNum.toFixed(2)}${unit} (${percentText[2]})`}
-                  >
-                    <div className="nowrap" style={{ width: percentText[2] }}>
-                      {i18n.t('project:available')}
-                    </div>
-                  </Tooltip>
-                </div>
-                <Tooltip title={i18n.t('usage limit exceeded')}>
-                  <CustomIcon type="warning" className="overuse-tip ml-1" />
-                </Tooltip>
-              </div>
-            );
-          };
-          return (
-            <div style={{ minWidth: '200px' }}>
-              <div className="flex justify-between items-center">
-                <div style={{ width: '40px' }}>CPU:</div>
-                {renderBar(+cpuServiceUsed.toFixed(2), +cpuAddonUsed.toFixed(2), cpuQuota, 'Core')}
-              </div>
-              <div className="flex justify-between items-center">
-                <div style={{ width: '40px' }}>MEM:</div>
-                {renderBar(+memServiceUsed.toFixed(2), +memAddonUsed.toFixed(2), memQuota, 'GiB')}
-              </div>
-            </div>
-          );
-        },
-      },
       {
-        title: i18n.t('operations'),
+        title: i18n.t('project:statistics'),
         key: 'op',
         dataIndex: 'id',
-        width: 120,
-        fixed: 'right',
+        width: 80,
         render: (id, record) => {
           if (record.type === 'MSP') {
             return null;
@@ -258,7 +135,7 @@ export const ProjectList = () => {
                   goTo(`./${id}/dashboard`);
                 }}
               >
-                {i18n.t('org:dashboard')}
+                <IconChartHistogramTwo />
               </span>
             </div>
           );
@@ -269,15 +146,28 @@ export const ProjectList = () => {
   };
 
   const onSearch = (query: string) => {
-    getProjectList({ query, pageNo: 1 });
+    setSearchObj((prev) => ({ ...prev, query, pageNo: 1 }));
   };
 
+  const getList = React.useCallback(
+    (_search?: IState) => {
+      getProjectList({ ...searchObj, ..._search });
+    },
+    [searchObj, getProjectList],
+  );
+
+  React.useEffect(() => {
+    getList(searchObj);
+  }, [searchObj, getList]);
+
   const handleTableChange = (pagination: any, filters: any, sorter: any) => {
-    if (sorter.order) {
-      getProjectList({ pageNo: pagination.current, orderBy: sorter.field, asc: sorter.order === 'ascend' });
-    } else {
-      getProjectList({ pageNo: pagination.current });
-    }
+    setSearchObj((prev) => ({
+      ...prev,
+      pageNo: pagination.current,
+      pageSize: pagination.pageSize,
+      orderBy: sorter?.field && sorter?.order ? sorter.field : undefined,
+      asc: sorter?.order ? sorter.order === 'ascend' : undefined,
+    }));
   };
 
   return (
@@ -305,9 +195,10 @@ export const ProjectList = () => {
               current: pageNo,
               pageSize,
               total,
+              showSizeChanger: true,
+              pageSizeOptions: PAGINATION.pageSizeOptions,
             }}
             onChange={handleTableChange}
-            scroll={{ x: 1900 }}
           />
         </SearchTable>
       </Spin>
