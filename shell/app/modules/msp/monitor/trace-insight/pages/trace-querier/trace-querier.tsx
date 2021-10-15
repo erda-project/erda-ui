bfcf769021aab86f0acee821a07f56c80912bbe3
hujiahao-hjh <82502479+hujiahao-hjh@users.noreply.github.com>
2021-10-15 17:13:50 +0800

    fix(msp): trace-querier detail method display bug (#1593)

--- shell/app/modules/msp/monitor/trace-insight/pages/trace-querier/trace-querier.tsx
+++ shell/app/modules/msp/monitor/trace-insight/pages/trace-querier/trace-querier.tsx
@@ -111,6 +111,10 @@ const TraceInsightQuerier = () => {
       });
   }, [getTraceDetailContent, requestId]);
 
+  React.useEffect(() => {
+    form.setFieldsValue({ method });
+  }, [method, form]);
+
   const resetRequestTrace = () => {
     form.resetFields();
     clearRequestTraceParams();
