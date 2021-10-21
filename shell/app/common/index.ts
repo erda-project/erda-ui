4c99dd8bbd9aab2816cc7c366cfbdfac618fa8ce
michael8512 <linshenghui321@sina.com>
2021-10-21 13:24:12 +0800

    WIP: remove useless import

--- shell/app/common/index.ts
+++ shell/app/common/index.ts
@@ -10,7 +10,6 @@
 //
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
-import config from '../../tailwind.config';
 
 export { Icon } from './components/icon';
 export { ErdaIcon } from './components/erda-icon/erda-icon';
