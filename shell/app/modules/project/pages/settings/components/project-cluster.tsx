2edadcff764ff94209a42a8a0f3ba784076d4c27
hujiahao-hjh <hjh958030@126.com>
2021-10-25 14:06:47 +0800

    fix(dop): dependency issues caused by code merging

--- shell/app/modules/project/pages/settings/components/project-cluster.tsx
+++ shell/app/modules/project/pages/settings/components/project-cluster.tsx
@@ -12,7 +12,7 @@
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import React from 'react';
-import { Table, Pagination } from 'antd';
+import { Table, Tooltip } from 'antd';
 import { DOC_PROJECT_RESOURCE_MANAGE, WORKSPACE_LIST } from 'common/constants';
 import { SectionInfoEdit } from 'project/common/components/section-info-edit';
 import i18n from 'i18n';
