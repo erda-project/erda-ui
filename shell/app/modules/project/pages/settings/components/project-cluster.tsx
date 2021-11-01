7d44ebd058d02a14f981fa15a1f151d811d3f9de
hujiahao-hjh <hjh958030@126.com>
2021-11-01 16:45:57 +0800

    fix(dop): project clusters list unification of digital accuracy and width

--- shell/app/modules/project/pages/settings/components/project-cluster.tsx
+++ shell/app/modules/project/pages/settings/components/project-cluster.tsx
@@ -169,7 +169,9 @@ const ProjectCluster = ({ hasEditAuth }: IProps) => {
           align: 'center',
           render: (text: string, record: PROJECT.ICluster) => (
             <div className="flex items-center">
-              <span>{text ? `${text}${i18n.t('core')}` : `0${i18n.t('core')}`}</span>
+              <span className="text-left" style={{ width: 50 }}>
+                {text ? `${+(+text).toFixed(2)}${i18n.t('core')}` : `0${i18n.t('core')}`}
+              </span>
               <span className="flex-grow ml-2">{renderBar('cpu', record, i18n.t('core'))}</span>
             </div>
           ),
@@ -181,7 +183,9 @@ const ProjectCluster = ({ hasEditAuth }: IProps) => {
           align: 'center',
           render: (text: string, record: PROJECT.ICluster) => (
             <div className="flex items-center">
-              <span>{text ? `${text}GB` : '0GB'}</span>
+              <span className="text-left" style={{ width: 50 }}>
+                {text ? `${+(+text).toFixed(2)}GB` : '0GB'}
+              </span>
               <span className="flex-grow ml-2">{renderBar('mem', record, 'GB')}</span>
             </div>
           ),
