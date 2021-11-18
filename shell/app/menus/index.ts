d2639b9eb579dd45e8a0eb9dbe6d2f9b58dd25cc
Zero <mobius_pan@yeah.net>
2021-11-18 16:20:10 +0800

    feat(msp): repalce msp name (#2044)
    * feat(msp): repalce msp name
    
    * feat(common): fixed locale conflicts between dashboard and erda-ui

--- shell/app/menus/index.ts
+++ shell/app/menus/index.ts
@@ -50,7 +50,7 @@ export const getSubSiderInfoMap = () => {
     msp: {
       menu: getMspMenu(),
       detail: {
-        displayName: i18n.t('microservice platform'),
+        displayName: i18n.t('msp'),
       },
     },
     ecp: {
