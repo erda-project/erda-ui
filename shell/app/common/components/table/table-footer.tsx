5eb9f891f0742e9a78c9074afb3ec8efa1b638a8
hujiahao-hjh <hjh958030@126.com>
2021-11-18 15:09:17 +0800

    refactor(common): separate table and pagination components

--- shell/app/common/components/table/table-footer.tsx
+++ shell/app/common/components/table/table-footer.tsx
@@ -12,11 +12,11 @@
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import React from 'react';
-import { Dropdown, Button, Menu, Pagination, Popover, Input } from 'antd';
+import { Dropdown, Button, Menu } from 'antd';
+import Pagination from 'common/components/pagination';
 import { ErdaIcon } from 'common';
 import i18n from 'i18n';
-import { PAGINATION } from 'app/constants';
-import { IRowSelection, IRowActions, TablePaginationConfig, IPaginationProps } from './interface';
+import { IRowSelection, IRowActions, TablePaginationConfig } from './interface';
 
 interface IProps {
   rowSelection?: IRowSelection;
@@ -26,9 +26,6 @@ interface IProps {
 }
 
 const TableFooter = ({ rowSelection, pagination, hidePagination, onTableChange }: IProps) => {
-  const { current = 1, pageSize = PAGINATION.pageSize, pageSizeOptions = PAGINATION.pageSizeOptions } = pagination;
-  const [goToVisible, setGoToVisible] = React.useState(false);
-
   const batchMenu = () => {
     return (
       <Menu>
@@ -42,73 +39,6 @@ const TableFooter = ({ rowSelection, pagination, hidePagination, onTableChange }
       </Menu>
     );
   };
-
-  const paginationCenterRender = (
-    <Popover
-      content={
-        <PaginationJump
-          pagination={pagination}
-          onTableChange={onTableChange}
-          hiddenPopover={() => setGoToVisible(false)}
-        />
-      }
-      trigger="click"
-      getPopupContainer={(triggerNode) => triggerNode.parentElement as HTMLElement}
-      placement="top"
-      overlayClassName="pagination-jump"
-      visible={goToVisible}
-      onVisibleChange={setGoToVisible}
-    >
-      <div className="pagination-center bg-hover mx-1 px-3 rounded" onClick={() => setGoToVisible(true)}>
-        {pagination.total ? pagination.current : 0} /{' '}
-        {(pagination.total && pagination.pageSize && Math.ceil(pagination.total / pagination.pageSize)) || 0}
-      </div>
-    </Popover>
-  );
-
-  const paginationItemRender = (
-    page: number,
-    type: 'page' | 'prev' | 'next' | 'jump-prev' | 'jump-next',
-    originalElement: React.ReactElement<HTMLElement>,
-  ) => {
-    const { total } = pagination;
-    switch (type) {
-      case 'prev':
-        return (
-          <div className="bg-hover" onClick={() => current > 1 && onTableChange({ pageNo: current - 1 })}>
-            {originalElement}
-          </div>
-        );
-      case 'next':
-        return (
-          <div
-            className="bg-hover"
-            onClick={() => total && current < Math.ceil(total / pageSize) && onTableChange({ pageNo: current + 1 })}
-          >
-            {originalElement}
-          </div>
-        );
-      case 'page':
-        if (page === 1) {
-          return paginationCenterRender;
-        }
-        return null;
-      default:
-        return null;
-    }
-  };
-
-  const pageSizeMenu = (
-    <Menu>
-      {(pageSizeOptions || []).map((item: string | number) => {
-        return (
-          <Menu.Item key={item} onClick={() => onTableChange({ pageNo: 1, pageSize: item })}>
-            <span className="fake-link mr-1">{i18n.t('{size} items / page', { size: item })}</span>
-          </Menu.Item>
-        );
-      })}
-    </Menu>
-  );
 
   return (
     <div className="erda-table-footer flex justify-between">
@@ -136,58 +66,10 @@ const TableFooter = ({ rowSelection, pagination, hidePagination, onTableChange }
       )}
 
       {!hidePagination && (
-        <div className="erda-pagination flex items-center justify-end">
-          <Pagination {...pagination} showSizeChanger={false} size="small" itemRender={paginationItemRender} />
-          {pageSizeOptions?.length && (
-            <Dropdown
-              trigger={['click']}
-              overlay={pageSizeMenu}
-              align={{ offset: [0, 5] }}
-              overlayStyle={{ minWidth: 120 }}
-              getPopupContainer={(triggerNode) => triggerNode.parentElement as HTMLElement}
-            >
-              <span className="bg-hover px-3">{i18n.t('{size} items / page', { size: pageSize })}</span>
-            </Dropdown>
-          )}
+        <div className="flex items-center justify-end">
+          <Pagination {...pagination} onChange={(page, size) => onTableChange({ pageNo: page, pageSize: size })} />
         </div>
       )}
-    </div>
-  );
-};
-
-const PaginationJump = ({ pagination, onTableChange, hiddenPopover }: IPaginationProps) => {
-  const { total = 0, pageSize = PAGINATION.pageSize } = pagination;
-  const [value, setValue] = React.useState('');
-
-  const handleChange = React.useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {
-      const { value: val } = e.target;
-      if (!isNaN(Number(val)) && +val <= Math.ceil(total / pageSize) && +val > 0 && !val.includes('.')) {
-        setValue(val);
-      } else if (!val) {
-        setValue('');
-      }
-    },
-    [setValue, total, pageSize],
-  );
-
-  const jump = () => {
-    if (value) {
-      onTableChange({ pageNo: value });
-      setValue('');
-      hiddenPopover();
-    }
-  };
-
-  return (
-    <div className="flex items-center" onClick={(e) => e.stopPropagation()}>
-      {i18n.t('Go to page')}
-      <Input className="mx-2" style={{ width: 80 }} value={value} onChange={handleChange} onPressEnter={jump} />
-      <Button
-        type="primary"
-        size="small"
-        icon={<ErdaIcon type="enter" onClick={jump} fill="white" className="relative top-0.5" />}
-      />
     </div>
   );
 };
