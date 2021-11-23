4992a2aaf832bd142e513f9d6eb43f462375ac09
daskyrk <daskyrk@163.com>
2021-11-23 10:17:15 +0800

    fix: add license header

--- shell/app/common/utils/create-scroll-component.tsx
+++ shell/app/common/utils/create-scroll-component.tsx
@@ -1,3 +1,16 @@
+// Copyright (c) 2021 Terminus, Inc.
+//
+// This program is free software: you can use, redistribute, and/or modify
+// it under the terms of the GNU Affero General Public License, version 3
+// or later ("AGPL"), as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 import React, { useContext } from 'react';
 import { DndContext } from 'react-dnd';
 import { throttle } from 'lodash';
@@ -67,7 +80,7 @@ function createVerticalStrength(_buffer: number) {
     return 0;
   };
 }
-const DEFAULT_BUFFER = 300;
+const DEFAULT_BUFFER = 240;
 
 export default function createScrollingComponent(WrappedComponent: string) {
   const ScrollingComponent = ({
