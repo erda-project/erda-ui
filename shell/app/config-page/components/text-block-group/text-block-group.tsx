df843899674a424b6a1e03b3e6d9e51c00a5e217
daskyrk <daskyrk@163.com>
2021-12-10 14:01:54 +0800

    feat: update text-block-group style

--- shell/app/config-page/components/text-block-group/text-block-group.tsx
+++ shell/app/config-page/components/text-block-group/text-block-group.tsx
@@ -18,17 +18,24 @@ import './text-block-group.scss';
 
 const TextBlockGroup = (props: CP_TEXT_BLOCK_GROUP.Props) => {
   const { props: configProps, data } = props;
+  const firstRowNum = data.data[0]?.length;
 
   return (
     <div className={`cp-text-block-group flex flex-col overflow-y-auto p-4 ${getClass(configProps)}`}>
       {data.data.map((rows, rowKey: number) => {
         const isFirstRow = rowKey === 0;
         return (
-          <div key={rowKey} className={`cp-text-block-group-row flex items-center ${isFirstRow ? '' : 'mt-4'}`}>
+          <div
+            key={rowKey}
+            className={`cp-text-block-group-row flex items-center justify-between ${isFirstRow ? '' : 'mt-4'}`}
+          >
             {rows.map((item, idx: number) => {
               const isFirstItem = idx === 0;
               return <TextBlockInfo key={`${rowKey}-${idx}`} className={isFirstItem ? '' : 'ml-2'} {...item} />;
             })}
+            {new Array(firstRowNum - rows.length).fill(1).map((idx: number) => (
+              <div className="erda-text-block-info" key={idx} />
+            ))}
           </div>
         );
       })}
