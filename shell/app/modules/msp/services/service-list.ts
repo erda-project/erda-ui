d56e86a3da3093b6fbdfe9f7633f0f77cd2f7658
Zero <mobius_pan@yeah.net>
2021-12-15 19:54:26 +0800

    feat(msp): optimize service list (#2287)

--- shell/app/modules/msp/services/service-list.ts
+++ shell/app/modules/msp/services/service-list.ts
@@ -25,41 +25,6 @@ interface AnalyzerOverviewParams {
   serviceIds: string[];
 }
 
-interface View {
-  timestamp: number;
-  value: number;
-}
-
-interface Views {
-  type: string;
-  data: number;
-  view: View[];
-}
-
-interface List {
-  id: string;
-  language: string;
-  lastHeartbeat: string;
-  name: string;
-  views?: Views[];
-}
-
-interface ServicesData {
-  list: List[];
-  total: number;
-  pageNo: number;
-  pageSize: number;
-}
-
-interface AnalyzerData {
-  views: Views[];
-  serviceId: string;
-}
-
-interface AnalyzerOverviewData {
-  list: AnalyzerData[];
-}
-
 const apis = {
   getServices: {
     api: 'get@/api/msp/apm/services',
@@ -69,7 +34,7 @@ const apis = {
   },
 };
 
-export const getServices = apiCreator<(payload: ServicesParams) => ServicesData>(apis.getServices);
-export const getAnalyzerOverview = apiCreator<(payload: AnalyzerOverviewParams) => AnalyzerOverviewData>(
-  apis.getAnalyzerOverview,
-);
+export const getServices = apiCreator<(payload: ServicesParams) => MSP_SERVICES.SERVICE_LIST>(apis.getServices);
+export const getAnalyzerOverview = apiCreator<
+  (payload: AnalyzerOverviewParams) => { list: MSP_SERVICES.SERVICE_LIST_CHART[] }
+>(apis.getAnalyzerOverview);
