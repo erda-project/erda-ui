ed9bd5e2a9e50ad7c3194a0ed533064116c18c71
hujiahao-hjh <hjh958030@126.com>
2021-12-20 01:17:01 +0800

    fix(common): configurable filter tags select dropdown bug

--- shell/app/common/components/render-form-item/index.tsx
+++ shell/app/common/components/render-form-item/index.tsx
@@ -541,6 +541,7 @@ const TagsSelect = ({ size, options, value = [], onChange, itemProps }: TagsSele
       filterOption={(input, option) =>
         typeof option?.label === 'string' && option?.label.toLowerCase().indexOf(input.toLowerCase()) >= 0
       }
+      listItemHeight={0}
     >
       {typeof options === 'function'
         ? options()
