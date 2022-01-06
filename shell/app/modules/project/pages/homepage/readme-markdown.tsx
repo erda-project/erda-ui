85d32f6c0e5a1d463b60a41b9b701a329da3e492
sherotree <wytreeshero@163.com>
2022-01-06 16:18:01 +0800

    style: add edit button in markdown (#2525)
    * style: add edit button in markdown
    
    * refactor: use css to control the hover style on edit button
    
    * fix: set display none when not hover on markdonw
    
    * refactor: refactor code

--- shell/app/modules/project/pages/homepage/readme-markdown.tsx
+++ shell/app/modules/project/pages/homepage/readme-markdown.tsx
@@ -14,7 +14,7 @@
 import React from 'react';
 import i18n from 'core/i18n';
 import { Tooltip } from 'antd';
-import { MarkdownEditor, EditField } from 'common';
+import { MarkdownEditor, EditField, ErdaIcon } from 'common';
 import remarkGfm from 'remark-gfm';
 import { useUpdate } from 'common/use-hooks';
 import ReactMarkdown from 'react-markdown';
@@ -38,7 +38,6 @@ export const ReadMeMarkdown = ({ value, onChange, onSave, disabled, originalValu
     isEditing: false,
     expandBtnVisible: false,
   });
-
   const mdContentRef = React.useRef<HTMLDivElement>(null);
 
   React.useEffect(() => {
@@ -78,12 +77,21 @@ export const ReadMeMarkdown = ({ value, onChange, onSave, disabled, originalValu
     />
   ) : (
     <div
-      className="relative cursor-pointer rounded w-full"
-      onClick={() => updater.isEditing(true)}
+      className="relative cursor-pointer rounded w-full read-only-markdown"
       style={{ maxHeight: expanded ? '' : maxHeight }}
     >
       <div className="overflow-hidden" style={{ maxHeight: 'inherit' }}>
         <div ref={mdContentRef} className="md-content">
+          <Tooltip title={i18n.t('dop:click to edit')}>
+            <div
+              className={
+                'markdown-edit-button h-8 w-8 fixed bg-white rounded-2xl shadow-card justify-center items-center'
+              }
+              onClick={() => updater.isEditing(true)}
+            >
+              <ErdaIcon type="edit" size={16} className="text-default-4 hover:text-default-8" />
+            </div>
+          </Tooltip>
           <ReactMarkdown remarkPlugins={[remarkGfm]} components={{ img: ScalableImage }}>
             {value || i18n.t('no description yet')}
           </ReactMarkdown>
