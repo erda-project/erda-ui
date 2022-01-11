fd20a28c15e6c152b9992f0e0379fe7ff247040d
daskyrk <daskyrk@163.com>
2022-01-11 15:26:33 +0800

    fix: update land page text

--- shell/app/layout/common/error-page.tsx
+++ shell/app/layout/common/error-page.tsx
@@ -134,68 +134,6 @@ const NotFound = ({ message, force }: { message?: string; force?: boolean }) => 
   );
 };
 
-const NotJoinOrg = () => {
-  const [orgs] = userStore.useStore((s) => [s.orgs]);
-  const [activeOrg, setActiveOrg] = React.useState<any>(null);
-  const [showOptions, setShowOptions] = React.useState(false);
-  const [filterKey, setFilterKey] = React.useState('');
-
-  const filteredList = orgs.filter((org) => org.displayName?.toLowerCase().includes(filterKey.toLowerCase()));
-
-  return (
-    <div className="land-page flex items-center justify-center h-full">
-      <div className="absolute left-20 top-5">
-        <ErdaIcon className="text-white" size={60} type="erda" />
-      </div>
-      <img className="bg-image" src={springBg} alt="background-image" />
-      <div className="content text-white z-10">
-        <div className="title">
-          <div>{i18n.t('layout:Global optimization')}</div>
-          <div>{i18n.t('layout:Help enterprises to create an agile R & D organization')}</div>
-        </div>
-        <div className="mt-8 org-select-text">{i18n.t('layout:Choose your organization space')}</div>
-        <div
-          className={`mt-4 rounded-sm h-16 py-5 text-default cursor-pointer flex items-center justify-between org-select  ${
-            showOptions ? 'showOptions' : ''
-          }`}
-        >
-          <input
-            className="input"
-            type="text"
-            value={activeOrg?.displayName || filterKey}
-            onChange={(e) => setFilterKey(e.target.value)}
-            onFocus={() => setShowOptions(true)}
-            onBlur={() => setShowOptions(false)}
-          />
-          <div className="tip text-default-6">{i18n.t('layout:Organizational space')}</div>
-          <ErdaIcon className="icon mr-6" size={20} type="caret-down" />
-          <div className="options">
-            {filteredList.map((org) => {
-              return (
-                <a key={org.id} href={`/${org.name}`}>
-                  <div
-                    className={`option flex items-center px-2 h-[76px] cursor-pointer hover:bg-default-04 ${
-                      org.id === activeOrg?.id ? 'active' : ''
-                    }`}
-                    onMouseEnter={() => setActiveOrg(org)}
-                    onMouseLeave={() => setActiveOrg(null)}
-                  >
-                    <img className="w-10 h-10 rounded-sm mr-2" src={org.logo} alt="" />
-                    <div>
-                      <div className="org-name">{org.displayName}</div>
-                      <div className="org-sub-name text-xs text-desc">{org.desc}</div>
-                    </div>
-                  </div>
-                </a>
-              );
-            })}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
 const FreeUserTips = () => {
   return (
     <div className="basic-error-page">
@@ -214,4 +152,4 @@ const FreeUserTips = () => {
   );
 };
 
-export { NoAuth, NotFound, NotJoinOrg, FreeUserTips };
+export { NoAuth, NotFound, FreeUserTips };
