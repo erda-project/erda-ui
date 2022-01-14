84df7e52fca07328b746e8dfca76dc911f5dd665
daskyrk <daskyrk@163.com>
2022-01-14 14:04:12 +0800

    feat: replace $color-table-head-bg with $color-default-02

--- shell/app/modules/project/pages/apps/app-list.tsx
+++ shell/app/modules/project/pages/apps/app-list.tsx
@@ -54,7 +54,7 @@ export const ProjectAppList = () => {
           },
           filter: {
             props: {
-              className: 'px-4 py-2',
+              className: 'px-4 py-2 bg-default-02',
             },
           },
         }}
