f46bcab49de77ff8dad84d99fb7f46e1af1c288b
hujiahao-hjh <hjh958030@126.com>
2022-01-14 18:50:31 +0800

    fix(dop): project pipeline style optimization

--- shell/app/menus/project.tsx
+++ shell/app/menus/project.tsx
@@ -103,8 +103,9 @@ export const getProjectMenu = (projectId: string, pathname: string) => {
         },
         {
           text: i18n.t('pipeline'),
-          href: goTo.resolve.projectPipeline(),
+          href: goTo.resolve.projectPipelineList(),
           show: projectPerm.pipeline.view.pass,
+          prefix: `${goTo.resolve.projectPipeline()}/`,
         },
         {
           href: goTo.resolve.projectReleaseList(),
