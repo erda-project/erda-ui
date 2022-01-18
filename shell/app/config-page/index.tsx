31f2febc2445f1b59697a75cee30bfab4e6e9278
hujiahao-hjh <hjh958030@126.com>
2022-01-18 20:25:43 +0800

    fix(common): config page inParams update bug

--- shell/app/config-page/index.tsx
+++ shell/app/config-page/index.tsx
@@ -208,6 +208,7 @@ const ConfigPage = React.forwardRef((props: IProps, ref: any) => {
       .then((res: CONFIG_PAGE.RenderConfig) => {
         const _curConfig = pageConfigRef.current;
         const newConfig = produce(_curConfig, (draft) => {
+          draft.inParams = reqConfig.inParams;
           draft.protocol = {
             ...draft.protocol,
             ...res.protocol,
