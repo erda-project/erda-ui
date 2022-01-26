85a67da3677afd9cbf7496a775c61dfa652bdadc
老俊 <782803291@qq.com>
2022-01-26 14:23:41 +0800

    fix: delete useless

--- shell/app/config-page/components/gantt/components/task-item/bar/bar.tsx
+++ shell/app/config-page/components/gantt/components/task-item/bar/bar.tsx
@@ -58,7 +58,6 @@ export const Bar: React.FC<TaskItemProps> = ({
     <g className={'erda-gantt-bar-wrapper'} tabIndex={0}>
       {isDateChangeable && (
         <foreignObject
-          transform={`translate(${task.x1 - 14},${task.y})`}
           onMouseDown={(e) => {
             isDateChangeable && onEventStart('move', task, e);
           }}
