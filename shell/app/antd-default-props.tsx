35e5f7c80add25e80405ab8421eb76804fc96b75
随风 <daskyrk@users.noreply.github.com>
2022-01-29 11:02:55 +0800

    feat: add default delay for loading (#2856)

---
+++ shell/app/antd-default-props.tsx
@@ -0,0 +1,36 @@
+// Copyright (c) 2021 Terminus, Inc.
+//
+// This program is free software: you can use, redistribute, and/or modify
+// it under the terms of the GNU Affero General Public License, version 3
+// or later ("AGPL"), as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import React from 'react';
+import { Pagination, Modal, Spin } from 'antd';
+import { Loading } from 'common';
+import { isZh } from 'i18n';
+import { PAGINATION } from './constants';
+
+Pagination.defaultProps = {
+  showSizeChanger: false,
+  ...Pagination.defaultProps,
+  pageSize: PAGINATION.pageSize,
+  pageSizeOptions: PAGINATION.pageSizeOptions,
+  showTotal: (total) => (isZh() ? `共计 ${total} 条` : `total ${total} items`),
+};
+
+Modal.defaultProps = {
+  ...Modal.defaultProps,
+  centered: true,
+};
+
+Spin.defaultProps = {
+  delay: 100,
+};
+Spin.setDefaultIndicator(<Loading />);
