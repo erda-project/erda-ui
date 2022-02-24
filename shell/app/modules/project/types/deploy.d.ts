5f9aa661d82fd8e5f9e737bd17d80ffc1af15d5f
Zero <mobius_pan@yeah.net>
2022-02-24 11:49:01 +0800

    fix(dop): fix preCheckResult validation

--- shell/app/modules/project/types/deploy.d.ts
+++ shell/app/modules/project/types/deploy.d.ts
@@ -155,7 +155,7 @@ declare namespace PROJECT_DEPLOY {
     id: string;
     name: string;
     releaseInfo: ReleaseInfo;
-    applicationsInfo: IApplicationsInfo[];
+    applicationsInfo: IApplicationsInfo[][];
   }
   interface IApplicationsInfo {
     id: string;
