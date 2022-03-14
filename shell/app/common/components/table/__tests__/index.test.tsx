ffad7c7a62de4c4a7e1006a044ec66e0d99add20
Zero <mobius_pan@yeah.net>
2022-03-14 16:15:44 +0800

    feat(test): add test case for ErdaTable

---
+++ shell/app/common/components/table/__tests__/index.test.tsx
@@ -0,0 +1,300 @@
+// Copyright (c) 2021 Terminus, Inc.
+//
+// This program is free software: you can use, redistribute, and/or modify
+// it under the terms of the GNU Affero General Public License, version 3
+// or later ("AGPL"), as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import React from 'react';
+import { fireEvent, render, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { ColumnProps, IActions } from '../interface';
+import ErdaTable from '..';
+
+interface IData {
+  id: string;
+  name: string;
+  title: string;
+  count: number;
+}
+
+describe('ErdaTable', () => {
+  const dataSource = [
+    {
+      id: '1',
+      name: 'erda',
+      title: 'Erda',
+      count: 1,
+    },
+    {
+      id: '2',
+      name: 'daily',
+      title: 'Daily',
+      count: 2,
+    },
+    {
+      id: '3',
+      name: 'test',
+      title: 'Test',
+      count: 0,
+    },
+  ];
+  const columns: ColumnProps<IData>[] = [
+    {
+      title: 'ID',
+      dataIndex: 'id',
+    },
+    {
+      title: 'name',
+      dataIndex: 'name',
+      subTitle: 'projectName',
+    },
+    {
+      title: 'Rule-Title',
+      dataIndex: 'title',
+    },
+    {
+      title: 'count',
+      align: 'right',
+      dataIndex: 'count',
+    },
+  ];
+  const genTableActions = (onClick: () => void) => {
+    const tableActions: IActions<IData> = {
+      render: (record) => {
+        return [
+          {
+            title: 'editAction',
+            onClick,
+          },
+          {
+            title: 'deleteAction',
+            show: record.count === 0,
+            onClick,
+          },
+        ];
+      },
+    };
+    return tableActions;
+  };
+
+  const defaultPagination = {
+    total: 99,
+    pageSize: 10,
+    current: 1,
+  };
+
+  it('should render well', () => {
+    const pageChangeFn = jest.fn();
+    const tableChangeFn = jest.fn();
+    const newPagination = { ...defaultPagination, current: 2, onChange: pageChangeFn };
+    const result = render(<ErdaTable columns={columns} dataSource={[]} hideHeader />);
+    expect(result.container).isExit('.erda-table-filter', 0);
+    result.rerender(<ErdaTable columns={columns} dataSource={[]} />);
+    expect(result.container).isExit('.erda-table-filter', 1);
+    result.rerender(
+      <ErdaTable columns={columns} dataSource={[]} pagination={newPagination} onChange={tableChangeFn} />,
+    );
+    expect(result.getByText('This page has no data, whether to go')).toBeTruthy();
+    fireEvent.click(result.getByText('page 1'));
+    expect(pageChangeFn).toHaveBeenLastCalledWith(defaultPagination.current, defaultPagination.pageSize);
+    expect(tableChangeFn).toHaveBeenLastCalledWith(
+      { ...newPagination, current: 1 },
+      {},
+      {},
+      { action: 'paginate', currentDataSource: [] },
+    );
+  });
+  it('should work well with tableHeader', async () => {
+    const reloadFn = jest.fn();
+    const tableChangeFn = jest.fn();
+    const pageChangeFn = jest.fn();
+    const result = render(
+      <ErdaTable
+        rowKey="id"
+        columns={columns}
+        dataSource={dataSource}
+        pagination={defaultPagination}
+        onReload={reloadFn}
+      />,
+    );
+    fireEvent.click(result.container.querySelector('[name="refresh"]')!);
+    expect(reloadFn).toHaveBeenLastCalledWith(defaultPagination.current, defaultPagination.pageSize);
+    result.rerender(
+      <ErdaTable
+        rowKey="id"
+        columns={columns}
+        dataSource={dataSource}
+        pagination={{ ...defaultPagination, onChange: pageChangeFn }}
+        onChange={tableChangeFn}
+      />,
+    );
+    fireEvent.click(result.container.querySelector('[name="refresh"]')!);
+    expect(tableChangeFn).toHaveBeenLastCalledWith(
+      {
+        current: defaultPagination.current,
+        pageSize: defaultPagination.pageSize,
+      },
+      {},
+      {},
+      { action: 'paginate', currentDataSource: [] },
+    );
+    expect(pageChangeFn).toHaveBeenLastCalledWith(defaultPagination.current, defaultPagination.pageSize);
+    expect(result.queryByText(columns[2].title as string, { selector: 'th' })).not.toBeNull();
+    fireEvent.click(result.container.querySelector('[name="config"]')!);
+    await waitFor(() => expect(result.getByRole('tooltip')).toBeInTheDocument());
+    fireEvent.click(result.getAllByRole('checkbox')[2]);
+    fireEvent.change(result.getAllByRole('checkbox')[2], { target: { checked: true } });
+    expect(result.queryByText(columns[2].title as string, { selector: 'th' })).toBeNull();
+  });
+  it('should work well with actions', async () => {
+    const actionFn = jest.fn();
+    const result = render(
+      <ErdaTable
+        rowKey="id"
+        columns={columns}
+        dataSource={dataSource}
+        pagination={defaultPagination}
+        actions={genTableActions(actionFn)}
+      />,
+    );
+    expect(result.container).isExit('[name="more"]', dataSource.length);
+    fireEvent.click(result.container.querySelectorAll('[name="more"]')[0]);
+    await waitFor(() => expect(result.getByRole('menu')).toBeInTheDocument());
+    fireEvent.click(result.getByText('editAction').closest('li')!);
+    expect(actionFn).toHaveBeenCalled();
+  });
+  it('should work well with sort is Object', async () => {
+    const tableChangeFn = jest.fn();
+    const pageChangeFn = jest.fn();
+    const newColumns: ColumnProps<IData>[] = [
+      ...columns.slice(0, 3),
+      {
+        title: 'count',
+        dataIndex: 'count',
+        sorter: {
+          compare: (a, b) => a.count - b.count,
+        },
+      },
+    ];
+    const result = render(
+      <ErdaTable
+        rowKey="id"
+        columns={newColumns}
+        dataSource={dataSource}
+        pagination={{ ...defaultPagination, onChange: pageChangeFn }}
+        onChange={tableChangeFn}
+      />,
+    );
+    expect(result.container).isExit('[name="caret-down"]', 1);
+    fireEvent.click(result.container.querySelector('[name="caret-down"]')!);
+    await waitFor(() => expect(result.getByRole('menu')));
+    fireEvent.click(result.getByText('ascend').closest('li')!);
+    expect(tableChangeFn).toHaveBeenCalledTimes(0);
+    fireEvent.click(result.getByText('descend').closest('li')!);
+    expect(tableChangeFn).toHaveBeenCalledTimes(0);
+  });
+  it('should work well with sour is function', async () => {
+    const tableChangeFn = jest.fn();
+    const pageChangeFn = jest.fn();
+    const result = render(
+      <ErdaTable
+        rowKey="id"
+        columns={[
+          ...columns.slice(0, 3),
+          {
+            title: 'count',
+            dataIndex: 'count',
+            sorter: (a, b) => a.count - b.count,
+          },
+        ]}
+        dataSource={dataSource}
+        pagination={{ ...defaultPagination, current: undefined, onChange: pageChangeFn }}
+        onChange={tableChangeFn}
+      />,
+    );
+    fireEvent.click(result.container.querySelector('[name="caret-down"]')!);
+    await waitFor(() => expect(result.getByRole('menu')));
+    fireEvent.click(result.getByText('ascend').closest('li')!);
+    expect(tableChangeFn).toHaveBeenCalledTimes(0);
+    fireEvent.click(result.getByText('descend').closest('li')!);
+    expect(tableChangeFn).toHaveBeenCalledTimes(0);
+  });
+  it('should work well with sour is boolean', async () => {
+    const tableChangeFn = jest.fn();
+    const pageChangeFn = jest.fn();
+    const result = render(
+      <ErdaTable
+        rowKey="id"
+        columns={[
+          ...columns.slice(0, 3),
+          {
+            title: 'count',
+            dataIndex: 'count',
+            sorter: true,
+          },
+        ]}
+        dataSource={dataSource}
+        pagination={{ ...defaultPagination, current: undefined, onChange: pageChangeFn }}
+        onChange={tableChangeFn}
+      />,
+    );
+    fireEvent.click(result.container.querySelector('[name="caret-down"]')!);
+    await waitFor(() => expect(result.getByRole('menu')));
+    fireEvent.click(result.getByText('ascend').closest('li')!);
+    expect(tableChangeFn).toHaveBeenCalledTimes(1);
+    fireEvent.click(result.getByText('descend').closest('li')!);
+    expect(tableChangeFn).toHaveBeenCalledTimes(2);
+    fireEvent.click(result.getByText('cancel order').closest('li')!);
+    expect(tableChangeFn).toHaveBeenCalledTimes(3);
+  });
+  it('should work well with pagination', () => {
+    const tableChangeFn = jest.fn();
+    const pageChangeFn = jest.fn();
+    const result = render(
+      <ErdaTable
+        rowKey="id"
+        columns={columns}
+        dataSource={dataSource}
+        pagination={{ ...defaultPagination, onChange: pageChangeFn }}
+        onChange={tableChangeFn}
+      />,
+    );
+    fireEvent.click(result.container.querySelector('[name="right"]')!);
+    expect(tableChangeFn).toHaveBeenCalledTimes(1);
+    result.rerender(
+      <ErdaTable
+        rowKey="id"
+        columns={columns}
+        dataSource={dataSource}
+        pagination={{ ...defaultPagination, current: undefined, onChange: pageChangeFn }}
+        onChange={tableChangeFn}
+      />,
+    );
+    fireEvent.click(result.container.querySelector('[name="right"]')!);
+    expect(tableChangeFn).toHaveBeenCalledTimes(1);
+  });
+  it('should work well with rowSelection', () => {
+    const rowSelectionChangeFn = jest.fn();
+    const result = render(
+      <ErdaTable
+        rowKey={(item) => item.id}
+        columns={columns}
+        dataSource={dataSource}
+        rowSelection={{
+          onChange: rowSelectionChangeFn,
+          selectedRowKeys: [],
+        }}
+      />,
+    );
+    fireEvent.click(result.getAllByRole('checkbox')[1]);
+    fireEvent.change(result.getAllByRole('checkbox')[1], { target: { checked: true } });
+    expect(rowSelectionChangeFn).toHaveBeenCalledTimes(1);
+  });
+});
---
+++ shell/app/common/components/table/__tests__/table-footer.test.tsx
@@ -0,0 +1,53 @@
+// Copyright (c) 2021 Terminus, Inc.
+//
+// This program is free software: you can use, redistribute, and/or modify
+// it under the terms of the GNU Affero General Public License, version 3
+// or later ("AGPL"), as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import React from 'react';
+import { fireEvent, render } from '@testing-library/react';
+import TableFooter from '../table-footer';
+
+describe('TableFooter', () => {
+  const baseProps = {
+    pagination: { total: 10 },
+    hidePagination: true,
+    onTableChange: jest.fn(),
+    whiteFooter: true,
+    rowKey: 'id',
+    dataSource: [{ id: 1, name: 'erda' }],
+    onSelectChange: jest.fn(),
+  };
+  it('should render well without actions and pagination', () => {
+    const result = render(<TableFooter {...baseProps} />);
+    expect(result.container).isExit('.erda-pagination', 0);
+    expect(result.container).isExitClass('.erda-table-footer', 'bg-white');
+    expect(result.container).not.isExitClass('.erda-table-footer', 'bg-default-02');
+    expect(result.queryByText('selected 0 items')).toBeNull();
+  });
+  it('should render well with actions', () => {
+    const result = render(<TableFooter {...baseProps} rowSelection={{ actions: [] }} />);
+    expect(result.queryByText('selected 0 items')).not.toBeNull();
+  });
+  it('should render well with pagination', () => {
+    const tableChangeFn = jest.fn();
+    const result = render(
+      <TableFooter {...baseProps} hidePagination={false} pagination={{ total: 100 }} onTableChange={tableChangeFn} />,
+    );
+    fireEvent.click(result.container.querySelector('[name="right"]')!);
+    expect(tableChangeFn).toHaveBeenLastCalledWith({ pageNo: 2, pageSize: 10 });
+    expect(result.container).isExit('.erda-pagination', 1);
+  });
+  it('should render well without whiteFooter', () => {
+    const result = render(<TableFooter {...baseProps} whiteFooter={false} />);
+    expect(result.container).not.isExitClass('.erda-table-footer', 'bg-white');
+    expect(result.container).isExitClass('.erda-table-footer', 'bg-default-02');
+  });
+});
