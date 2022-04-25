dd6b2e18cda6d0b7d3b9da11fa69ab0787823ec1
陈韡韬 <mcdaddychen@126.com>
2022-04-25 13:55:33 +0800

    feat(components): add license

--- modules/components/.dumi/theme/layout.tsx
+++ modules/components/.dumi/theme/layout.tsx
@@ -1,4 +1,16 @@
-// .dumi/theme/layout.tsx(本地主题) 或 src/layout.tsx(主题包)
+// Copyright (c) 2021 Terminus, Inc.
+//
+// This program is free software: you can use, redistribute, and/or modify
+// it under the terms of the GNU Affero General Public License, version 3
+// or later ("AGPL"), as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 import React from 'react';
 import Layout from 'dumi-theme-default/es/layout';
 import ConfigProvider from '../../src/context-provider';
