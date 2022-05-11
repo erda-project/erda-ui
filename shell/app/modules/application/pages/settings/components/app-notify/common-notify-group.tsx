636a6702d4a1787274a2ad81369a70a71c2a854a
Zero <mobius_pan@yeah.net>
2022-05-11 10:44:58 +0800

    fix(msp): fix locale

--- shell/app/modules/application/pages/settings/components/app-notify/common-notify-group.tsx
+++ shell/app/modules/application/pages/settings/components/app-notify/common-notify-group.tsx
@@ -62,7 +62,7 @@ const hasChannelMethod = (channelMethods: typeof notifyChannelOptionsMap, method
 export const getFinalNotifyChannelOptions = (channels: Obj<string>, isMonitor: boolean) => {
   const VMSChannel = { name: i18n.t('Phone-tel'), value: 'vms' };
   const SMSChannel = { name: i18n.t('SMS'), value: 'sms' };
-  const dingdingWorkChannel = { name: i18n.t('DingTalk'), value: 'dingtalk_work_notice' };
+  const dingdingWorkChannel = { name: i18n.t('DingTalk-work-notice'), value: 'dingtalk_work_notice' };
   const channelMethods = cloneDeep(notifyChannelOptionsMap);
   forEach(channels, (_, key) => {
     if (key === 'vms' && !hasChannelMethod(channelMethods, 'vms')) {
