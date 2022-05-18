3830a3b9d5a2038b80021a916f9e84586612d1f2
陈韡韬 <mcdaddychen@126.com>
2022-05-18 19:51:23 +0800

    feat(components): add changelog & fix menu color issue

--- core/src/history.ts
+++ core/src/history.ts
@@ -10,24 +10,8 @@
 //
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
-import { createBrowserHistory, UnregisterCallback, LocationState, LocationListener } from 'history';
+import { createBrowserHistory } from 'history';
 
 const history = createBrowserHistory();
-
-const { listen } = history;
-
-// because of qiankun > single-spa rewrite pushState, lead to history.listen execute twice
-history.listen = (listener: LocationListener<LocationState>): UnregisterCallback => {
-  let lastPathname = '';
-  function enhancerCb(...args: any) {
-    const { pathname, search, hash } = window.location;
-    const curUrl = `${pathname}${search}${hash}`;
-    if (curUrl === lastPathname) return;
-    lastPathname = curUrl;
-    // @ts-ignore pass args directly
-    return listener(...args);
-  }
-  return listen(enhancerCb);
-};
 
 export default history;
