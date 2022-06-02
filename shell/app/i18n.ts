1be9918ac0244168a3323d16784e5948565bb92d
liyaoyao <litikky@126.com>
2022-06-02 16:59:34 +0800

    feat: rename to i18n-easy-edit

--- shell/app/i18n.ts
+++ shell/app/i18n.ts
@@ -12,8 +12,8 @@
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import i18n, { getCurrentLocale, getLang, isZh, setLocale } from 'core/i18n';
-import overwriteT from './i18n-page-edit/overwrite_i18n';
-import { isEditAccess } from './i18n-page-edit/utils';
+import overwriteT from './i18n-easy-edit/overwrite_i18n';
+import { isEditAccess } from './i18n-easy-edit/utils';
 import zh from './locales/zh.json';
 import en from './locales/en.json';
 import defaultZh from '../../locales/zh.json';
--- shell/app/layout/pages/page-container/components/shell/index.tsx
+++ shell/app/layout/pages/page-container/components/shell/index.tsx
@@ -13,7 +13,7 @@
 
 import React from 'react';
 import './index.scss';
-import I18nPageEdit from 'app/i18n-page-edit/pages';
+import I18nEasyEdit from 'app/i18n-easy-edit/pages';
 
 interface IProps {
   children?: React.ReactNode;
@@ -41,7 +41,7 @@ const Shell = ({ children, breadcrumb, announcement, navigation, sidebar, classN
         </div>
       )}
       <div className={`erda-main-content ${mainClassName || ''}`}>{children}</div>
-      <I18nPageEdit />
+      <I18nEasyEdit />
     </div>
   );
 };
