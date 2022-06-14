3b7a0858ab7975efc991882d5671e4255e889c14
zxj <782803291@qq.com>
2022-06-14 09:38:54 +0800

    fix: fix pipeline record params error (#3594)

--- shell/app/modules/project/services/pipeline.tsx
+++ shell/app/modules/project/services/pipeline.tsx
@@ -196,7 +196,7 @@ export const getGuidesList = apiCreator<(payload: { kind: string; projectID: str
 export const cancelGuide = apiCreator<(payload: { id: string }) => RAW_RESPONSE>(apis.cancelGuide);
 
 export const getPipelineRecord = apiCreator<
-  (payload: { projectID: string; pageNo: number; pageSize: number; name: string }) => {
+  (payload: { projectID: string; pageNo: number; pageSize: number; name: string; appNames: string }) => {
     execHistories: PipelineRecord[];
     total: number;
   }
