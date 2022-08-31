14e067c219458c13f4fc5153ea1a030259db427f
老俊 <782803291@qq.com>
2022-08-31 09:52:51 +0800

    fix: fix grafana url

--- shell/app/modules/cmp/pages/edge/index.tsx
+++ shell/app/modules/cmp/pages/edge/index.tsx
@@ -3,10 +3,7 @@ import React from 'react';
 export default () => {
   return (
     <div className="h-full w-full">
-      <iframe
-        style={{ border: 'none', width: '100%', height: '100%' }}
-        src="http://10.154.220.10/grafana/d/lLuq8-z7k/overview?from=1661776363450&to=1661778163450&orgId=1"
-      />
+      <iframe style={{ border: 'none', width: '100%', height: '100%' }} src="http://grafana-external.prod.ykjt.cc/" />
     </div>
   );
 };
