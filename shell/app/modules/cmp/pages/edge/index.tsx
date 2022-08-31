ce944ef9854292bab311d19b50d9194907769d16
老俊 <782803291@qq.com>
2022-08-31 14:38:37 +0800

    fix: fix url in iframe

--- shell/app/modules/cmp/pages/edge/index.tsx
+++ shell/app/modules/cmp/pages/edge/index.tsx
@@ -3,7 +3,10 @@ import React from 'react';
 export default () => {
   return (
     <div className="h-full w-full">
-      <iframe style={{ border: 'none', width: '100%', height: '100%' }} src="https://grafana-external.prod.ykjt.cc/" />
+      <iframe
+        style={{ border: 'none', width: '100%', height: '100%' }}
+        src="http://10.154.220.10/grafana/d/lLuq8-z7k/overview?from=1661776363450&to=1661778163450&orgId=1"
+      />
     </div>
   );
 };
