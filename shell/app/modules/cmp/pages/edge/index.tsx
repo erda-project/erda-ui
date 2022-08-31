45b540acb241d7e5d002197c2a9725fb975f3a2a
老俊 <782803291@qq.com>
2022-08-31 10:44:08 +0800

    fix: fix shanneng iframe url

--- shell/app/modules/cmp/pages/edge/index.tsx
+++ shell/app/modules/cmp/pages/edge/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default () => {
   return (
     <div className="h-full w-full">
-      <iframe style={{ border: 'none', width: '100%', height: '100%' }} src="http://grafana-external.prod.ykjt.cc/" />
+      <iframe style={{ border: 'none', width: '100%', height: '100%' }} src="https://grafana-external.prod.ykjt.cc/" />
     </div>
   );
 };
