bc8a510a5751af1c08788834da98cc1bec8811c8
hujiahao-hjh <82502479+hujiahao-hjh@users.noreply.github.com>
2023-08-29 10:53:01 +0800

    feat: ai chat replacement (#3901)

--- shell/app/layout/pages/page-container/components/chat-GPT/index.tsx
+++ shell/app/layout/pages/page-container/components/chat-GPT/index.tsx
@@ -12,14 +12,22 @@
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import React, { useState } from 'react';
+import { encode } from 'js-base64';
 import { Modal } from 'antd';
 import { ErdaIcon as CustomIcon } from 'common';
+import userStore from 'app/user/stores';
+import orgStore from 'app/org-home/stores/org';
 import Sidebar from './sidebar';
-import Chat from './chat';
+import { ChatProvider, Chat as TChart } from '@terminus/ai-components';
+import { erdaEnv } from 'common/constants';
+
+const AI_BACKEND_URL = erdaEnv.AI_BACKEND_URL;
 
 const ChatGPT = () => {
   const [visible, setVisible] = useState(false);
   const [currentChat, setCurrentChat] = useState<number>();
+  const { id: userId, nick: name, phone, email } = userStore.getState((s) => s.loginUser);
+  const orgId = orgStore.useStore((s) => s.currentOrg.id);
 
   return (
     <>
@@ -32,7 +40,51 @@ const ChatGPT = () => {
       <Modal visible={visible} width="60%" onCancel={() => setVisible(false)} footer={false} bodyStyle={{ padding: 0 }}>
         <div className="flex" style={{ height: '80vh' }}>
           <Sidebar onChange={setCurrentChat} />
-          <Chat id={currentChat} />
+
+          <div className="flex-1">
+            <ChatProvider>
+              <TChart
+                getMsgfetchConfig={{
+                  path: `${AI_BACKEND_URL}/v1/chat/completions`,
+                  getBody: (messages) => ({
+                    model: 'gpt-35-turbo-16k',
+                    messages: [
+                      {
+                        role: 'user',
+                        content: messages[messages?.length - 1].content,
+                      },
+                    ],
+                    stream: true,
+                  }),
+                  headers: {
+                    'X-Ai-Proxy-User-Id': encode(userId),
+                    'X-Ai-Proxy-Username': encode(name),
+                    'X-Ai-Proxy-Phone': encode(phone),
+                    'X-AI-Proxy-Email': encode(email),
+                    'X-Ai-Proxy-Source': 'erda.cloud',
+                    'X-Ai-Proxy-Org-Id': encode(`${orgId}`),
+                    'X-AI-Proxy-SessionId': `${currentChat}`,
+                  },
+                  formatResult: (msgData) => {
+                    const { data } = msgData;
+                    if (data !== '[DONE]') {
+                      const dataObj = JSON.parse(data);
+                      const { choices } = dataObj;
+                      const { delta } = choices[0];
+                      const { content } = delta;
+                      return { data: content || '' };
+                    }
+                    return { data: '' };
+                  },
+                }}
+                toolbarConfig={{
+                  historyConfig: { show: false },
+                  promptConfig: { show: false },
+                  chatConfig: { show: false },
+                }}
+              />
+            </ChatProvider>
+          </div>
         </div>
       </Modal>
     </>
