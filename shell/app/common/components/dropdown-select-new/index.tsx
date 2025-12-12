ebae649d6d46912496615996d0a5a8aeccf5232b
老俊 <782803291@qq.com>
2025-12-12 15:50:35 +0800

    fix: 修复theme

--- shell/app/common/components/dropdown-select-new/index.tsx
+++ shell/app/common/components/dropdown-select-new/index.tsx
@@ -172,7 +172,7 @@ const DropdownSelect = (props: DropdownSelectNewProps) => {
       overlay={overlay}
       visible={active}
       placement="bottomRight"
-      trigger={trigger || ['click']}
+      trigger={['click']}
       onVisibleChange={(v) => {
         if (trigger?.includes('hover')) {
           setActive(v);
